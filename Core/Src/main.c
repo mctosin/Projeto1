033d710b81161dc9496050d1a508017ddb4dcb9c
mctosin <39802158+mctosin@users.noreply.github.com>
2026-04-15 12:22:58 -0300

    pwm cagado

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,71 +96,71 @@ void ST_def_lapse(type_Soft_Timer* ptimer, uint32_t tlapse);
 
 type_PWM pwm_orange; 
 
-type_Soft_Timer timer_PWM_update;
+type_Soft_Timer timer_U;
+float duty_cycle_U;
+type_bool flag_increase_U;
+	
 
 void PWM_run(type_PWM* pwm);
 
-void PWM_update(type_PWM* pwm)
+void PWM_update(type_PWM* pwm);
+
+	///////PWM Professor///////
+void PWM_initialize(type_PWM* pwm, GPIO_TypeDef* port, uint16_t pin, 
+	uint32_t t_period, float duty_cycle);
+
+void PWM_def_parameters(type_PWM* pwm, uint32_t t_period, float duty_cycle);
+	///////PWM Professor///////
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
 {
-	pwm->t_active = (uint32_t)(pwm->duty_cycle * pwm->t_period);
-	pwm->t_inactive = pwm->t_period - pwm->t_active;
-}
-	///////PWM Professor///////
-	void PWM_initialize(type_PWM* pwm, GPIO_TypeDef* port, uint16_t pin, uint32_t t_period, float duty_cycle);
 
-	void PWM_def_parameters(type_PWM* pwm, uint32_t t_period, float duty_cycle);
-	///////PWM Professor///////
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END PFP */
+  /* USER CODE END 1 */
 
-	/* Private user code ---------------------------------------------------------*/
-	/* USER CODE BEGIN 0 */
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* USER CODE END 0 */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/**
-	  * @brief  The application entry point.
-	  * @retval int
-	  */
-	int main(void)
-	{
+  /* USER CODE BEGIN Init */
 
-		/* USER CODE BEGIN 1 */
+  /* USER CODE END Init */
 
-		/* USER CODE END 1 */
+  /* Configure the system clock */
+  SystemClock_Config();
 
-		/* MCU Configuration--------------------------------------------------------*/
+  /* USER CODE BEGIN SysInit */
 
-		/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-		HAL_Init();
+  /* USER CODE END SysInit */
 
-		/* USER CODE BEGIN Init */
-
-		/* USER CODE END Init */
-
-		/* Configure the system clock */
-		SystemClock_Config();
-
-		/* USER CODE BEGIN SysInit */
-
-		/* USER CODE END SysInit */
-
-		/* Initialize all configured peripherals */
-		MX_GPIO_Init();
-		/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
 	
-		  //HAL_TIM_Base_Start_IT(&htim2);
-		  //HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
+	
 		ST_initialize(&timer_RED, 500);
-		//HAL_GPIO_TogglePin(BLUE_LED_GPIO_Port, BLUE_LED_Pin);
 		ST_initialize(&timer_BLUE, 625);
-		/*t_lapse = 500;
-		t_lapse2 = 400;
-		t_initial = HAL_GetTick();*/
+		ST_initialize(&timer_U, 1000);
+	
+		duty_cycle_U = 0.0f;	
+		flag_increase_U = True;
 	
 		//////////// PWM PROFESSOR ///////////////
 	
-		PWM_initialize(&pwm_orange, ORANGE_LED_GPIO_Port, ORANGE_LED_Pin, 1000, 1.0f);
+		PWM_initialize(&pwm_orange, ORANGE_LED_GPIO_Port, ORANGE_LED_Pin, 1000, 0.2f);
 	
 		//////////// PWM PROFESSOR ///////////////
 	
@@ -168,80 +168,107 @@ void PWM_update(type_PWM* pwm)
 		//HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_SET);
 		//HAL_GPIO_WritePin(BLUE_LED_GPIO_Port, BLUE_LED_Pin, GPIO_PIN_SET);
 
-	  /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	  /* Infinite loop */
-	  /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 		while (1)
 		{
-			/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-			/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	  
 			if (ST_run(&timer_BLUE))
 			{
 				HAL_GPIO_TogglePin(BLUE_LED_GPIO_Port, BLUE_LED_Pin);
 				ST_lapse(&timer_BLUE);
 			}
-	  
+#if 0	  
 			if (ST_run(&timer_RED))
 			{
 				HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
 				ST_lapse(&timer_RED);
 			}
-	 
+			
+
+			if (ST_run(&timer_U))
+			{
+				ST_lapse(&timer_U);
+				if (flag_increase_U)
+				{
+					duty_cycle_U += 0.2f;
+					if (duty_cycle_U >= 1.0f)
+					{
+						duty_cycle_U = 1.0f;
+						flag_increase_U = False;
+					}
+				}
+				else
+				{
+					duty_cycle_U -= 0.2f;
+					if (duty_cycle_U <= 0.0f)
+					{
+						duty_cycle_U = 0.0f;
+						flag_increase_U = True;
+					}
+				}
+				
+				PWM_def_parameters(&pwm_orange, 1000, duty_cycle_U);
+			}
+#endif
 			PWM_run(&pwm_orange);
+			
 	    
 		}
-		/* USER CODE END 3 */
-	}
+  /* USER CODE END 3 */
+}
 
-	/**
-	  * @brief System Clock Configuration
-	  * @retval None
-	  */
-	void SystemClock_Config(void)
-	{
-		RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-		RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-		/** Configure the main internal regulator output voltage
-		*/
-		__HAL_RCC_PWR_CLK_ENABLE();
-		__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-		/** Initializes the RCC Oscillators according to the specified parameters
-		* in the RCC_OscInitTypeDef structure.
-		*/
-		RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-		RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-		RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-		RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-		RCC_OscInitStruct.PLL.PLLM = 4;
-		RCC_OscInitStruct.PLL.PLLN = 168;
-		RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-		RCC_OscInitStruct.PLL.PLLQ = 4;
-		if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-		{
-			Error_Handler();
-		}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-		/** Initializes the CPU, AHB and APB buses clocks
-		*/
-		RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-		                            | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-		RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-		RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-		RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-		RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-		if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-		{
-			Error_Handler();
-		}
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
-	/* USER CODE BEGIN 4 */
+/* USER CODE BEGIN 4 */
 
 	type_bool ST_run(type_Soft_Timer* ptimer)
 	{
@@ -267,43 +294,38 @@ void PWM_update(type_PWM* pwm)
 		ptimer->t_lapse = tlapse;
 	}
 
-
-	///////PWM Professor///////
 	void PWM_run(type_PWM* pwm)
 	{
-		switch (pwm->state)
+		if (ST_run(&pwm->timer))
 		{
-		case active:
-			if (ST_run(&pwm->timer))
+			if (pwm->state)
 			{
+				// Ativo
 				HAL_GPIO_WritePin(pwm->port, pwm->pin, GPIO_PIN_RESET);
 				ST_def_lapse(&pwm->timer, pwm->t_inactive);
+				ST_lapse(&pwm->timer);
 				pwm->state = inactive;
 			}
-			break;
-			pwm->timer.t_initial = HAL_GetTick();
-		case inactive:
-			if (ST_run(&pwm->timer))
+			else
 			{
+				//Inativo
 				HAL_GPIO_WritePin(pwm->port, pwm->pin, GPIO_PIN_SET);
 				ST_def_lapse(&pwm->timer, pwm->t_active);
+				ST_lapse(&pwm->timer);
 				pwm->state = active;
 			}
-			break;
-			pwm->timer.t_initial = HAL_GetTick();
-		default:
-			break;
-			pwm->timer.t_initial = HAL_GetTick();
 		}
+
 	}
-	void PWM_initialize(type_PWM* pwm, GPIO_TypeDef* port, uint16_t pin, uint32_t t_period, float duty_cycle)
+	void PWM_initialize(type_PWM* pwm, GPIO_TypeDef* port, uint16_t pin, 
+						uint32_t t_period, float duty_cycle)
 	{
 		PWM_def_parameters(pwm, t_period, duty_cycle);
 		pwm->port = port;
 		pwm->pin = pin;
 		pwm->state = active;
 		HAL_GPIO_WritePin(pwm->port, pwm->pin, GPIO_PIN_SET);
-		ST_initialize(&pwm->timer, pwm->t_active);
+		ST_initialize(&(pwm->timer), pwm->t_active);
 	}
 
 	void PWM_def_parameters(type_PWM* pwm, uint32_t t_period, float duty_cycle)
@@ -325,37 +347,39 @@ void PWM_update(type_PWM* pwm)
 		pwm->t_inactive = pwm->t_period - pwm->t_active;
 	
 	}
+
+
 	///////PWM Professor///////
 
-	/* USER CODE END 4 */
+/* USER CODE END 4 */
 
-	/**
-	  * @brief  This function is executed in case of error occurrence.
-	  * @retval None
-	  */
-	void Error_Handler(void)
-	{
-		/* USER CODE BEGIN Error_Handler_Debug */
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 		/* User can add his own implementation to report the HAL error return state */
 		__disable_irq();
 		while (1)
 		{
 		}
-		/* USER CODE END Error_Handler_Debug */
-	}
-	#ifdef USE_FULL_ASSERT
-	/**
-	  * @brief  Reports the name of the source file and the source line number
-	  *         where the assert_param error has occurred.
-	  * @param  file: pointer to the source file name
-	  * @param  line: assert_param error line source number
-	  * @retval None
-	  */
-	void assert_failed(uint8_t *file, uint32_t line)
-	{
-		/* USER CODE BEGIN 6 */
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
 		/* User can add his own implementation to report the file name and line number,
 		   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-		 /* USER CODE END 6 */
-	}
-	#endif /* USE_FULL_ASSERT */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
